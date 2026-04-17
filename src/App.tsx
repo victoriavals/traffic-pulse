db9542165a3ff3b41bd5f7656646629c578263c0
victoriavals <naufalfirdaus282@gmail.com>
2026-04-17 09:42:23 +0700

    chore(theme): change default theme from system to light in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ import NotFound from "./pages/NotFound";
 const queryClient = new QueryClient();
 
 const App = () => (
-  <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange={false}>
+  <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange={false}>
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
